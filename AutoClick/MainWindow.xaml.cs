bfba31c1ff56afb6d20a973e5c4ec64d1117e786
Zae <zaevi@live.com>
2019-07-24 15:51:43 +0800

    更新引用

--- AutoClick/MainWindow.xaml.cs
+++ AutoClick/MainWindow.xaml.cs
@@ -1,20 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using GalaScript;
+using System;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using GalaScript;
 
 
 namespace AutoClick
@@ -54,7 +43,7 @@ namespace AutoClick
         {
             if(_task != null)
             {
-                _engine.Paused = true;
+                _engine.Pause();
                 _cts.Cancel();
                 _task = null;
             }
--- AutoClick/Simulator.cs
+++ AutoClick/Simulator.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using GalaScript.Interfaces;
+using GalaScript.Abstract;
 using GalaScript;
 using WindowsInput;
 using System.Windows;
